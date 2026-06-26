394af7033bfef36e972f66d85dcff855a8ec9b0d
SharanyaDN-art <Sharanyadn62@gmail.com>
2026-06-26 11:01:14 +0530

    Initial commit

---
+++ EDA_project.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import os
+
+print("understanding dataset")
+
+file_name="sales_data.csv"
+if not os.path.exists(file_name):
+    print(f"Error:{file_name}is not found")
+
+df=pd.read_csv(file_name)
+print("succesfully loaded")
+print(f"Shape of the dataset: Rows: {df.shape[0]}, Columns: {df.shape[1]}")
+
+print(df.head())
+print(df.tail())
+print(df.describe())
+
